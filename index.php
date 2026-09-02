dbc33b1679bfa3786f24389cf5eaf74f43d1eb04
Artur Erik Pöder <artur.poder1@gmail.com>
2026-09-02 13:21:00 +0300

    echo age ja name example

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-echo "A Rare Gift";
+echo "A Rare Gift"; // Trent PLS VAATA JJK
+$name = "Artur"; // Parim inimene eales
+$age = 30; // Arturi uus vanus
+echo $age;
+$age = $age * 2 * 3 * 4 * 5 * 6 * 7;
+echo $age;
+
+echo 67 + 69; // Annab selle numbri väärtuse heheheh
 ?>
