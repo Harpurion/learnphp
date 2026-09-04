bc05402051c80e79f84ffbdd63e49fddbe2f4975
Artur Erik Pöder <artur.poder1@gmail.com>
2026-09-04 13:06:56 +0300

    math bullshit

--- index.php
+++ index.php
@@ -1,20 +1,17 @@
 <?php
 $test = 10;
-$test = 3.1;
-$test = "Hello";
-$test = "😂";
-$test = true;
-$test = false;
-$test = null;
-$lol;
+$test = 1 + 2;
+$test = 1 - 5;
+$test = 3 * 9;
+$test = 4 / 4;
+$test = $test - 1; //samad
+$test -= 1; //samad
+$test += 1;
+$test *= 1;
+$test /= 1;
+$test = 10 % 5;
+$test++; 
+$test--; //samad
+$test = (445 + (4 - 9) * 3) / 2;
+$test %= 67;
 var_dump($test);
-var_dump($lol);
-$test = [1, 2, "three", true];
-var_dump($test);
-$test = new stdClass();
-$test->name = "John";
-var_dump($test);
-$num1 = 100000000;
-$num2 = "67";
-var_dump($num1 === $num2);
-?>
