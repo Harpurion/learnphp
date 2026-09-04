07aad9ccee3330a0ef87755a7ad6a35a65d38b0b
Artur Erik Pöder <artur.poder1@gmail.com>
2026-09-04 14:12:34 +0300

    True or false

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php
-$test = 10;
-$test = 1 + 2;
-$test = 1 - 5;
-$test = 3 * 9;
-$test = 4 / 4;
-$test = $test - 1; //samad
-$test -= 1; //samad
-$test += 1;
-$test *= 1;
-$test /= 1;
-$test = 10 % 5;
-$test++; 
-$test--; //samad
-$test = (445 + (4 - 9) * 3) / 2;
-$test %= 67;
+$test = true;
+$test = false;
+$test = true && false;
+$test = true && true;
+$test = true || false;
+$test = !false;
+$test = 100 > 5;
+$test = 67 > 67;
+$test = 55 == 55;
+$test = 55 === 55;
+$test = 56 >= 56;
+$test = 10 == '10';
+$test = 10 === '10';
+$test = true && false || (true && !false);
 var_dump($test);
+
+// kaks võrdus (==) tähendab, et arvuti käest küsid kas on võrdne arv?
+// kolm võrdus (===) tähendab, et arvuti käest küsid kas on võrdne arv ja tüüp?
