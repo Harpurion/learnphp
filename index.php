6270e730b6d50223b35fac0420566b8fd7c6d8a6
Artur Erik Pöder <artur.poder1@gmail.com>
2026-09-11 13:30:50 +0300

    add magic methods

--- index.php
+++ index.php
@@ -1,22 +1,41 @@
 <?php
 
-class box {
-    public $width;
-    public $height;
-    public $length;
-    public $isOpen = false;
-    public $hasBeenOpened = false;
+class Box {
 
-    public function Open() {
-        $this->isOpen = true;
-        $this->hasBeenOpened = true;
+
+    public function __construct(private $w, private $h, private $l) {
+        
+        var_dump('Poop was created');
     }
 
-     public function close() {
-        $this->isOpen = false;
+    public function volume(){
+        return $this->w * $this->h * $this->l;
     }
 
-    public function volume() {
-        return $this->width * $this->height * $this->length;
+    public function __set($name, $value) {
+        var_dump($name, $value);
+    }
+
+    public function __toString() {
+        return "I'm a poopie W: $this->w H: $this->h L: $this->l";
+    }
+
+    public function __destruct() {
+        var_dump('Poop was destroyed');
     }
 }
+
+function lol() {
+    $box4 = new Box(5, 7, 9);
+}
+
+lol();
+
+$box1 = new Box(57, 23, 12);
+$box4 = $box1;
+$box1 = 57;
+$box2 = new Box(10, 20, 30);
+var_dump($box1, $box2);
+$box3 = clone $box2;
+var_dump($box3);
+echo $box1;
