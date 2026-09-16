39f38f5a1071c48a48efa193914a013fdf9beb04
Harpurion <artur.poder1@gmail.com>
2026-09-16 09:31:04 +0300

    Add new stuff to index.php in public and db, public controller

---
+++ public/index.php
@@ -0,0 +1,91 @@
+<?php
+if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER["REQUEST_URI"])) {
+    return false;    // serve the requested resource as-is.
+}
+function dump(...$vars)
+{
+    echo '<pre>';
+    var_dump(...$vars);
+    echo '</pre>';
+}
+
+spl_autoload_register(function ($sitt){
+    $sitt = substr($sitt,4);
+    $sitt = str_replace('\\', '/', $sitt);
+    require_once __DIR__ . '/../src/' . $sitt . '.php';
+});
+
+use App\Controllers\PublicController as SHORTCUT; // "as" on alias 
+
+// require_once __DIR__ . '/../src/Router.php';
+// require_once __DIR__ . '/../src/DB.php';
+
+$controller = new SHORTCUT();
+$router = new App\Router();
+$db = new App\DB();
+dump($router, $db);
+
+// switch ($_SERVER['REQUEST_URI']) {
+//     case '/':
+//         $title = 'World';
+//         $posts = [
+//             [
+//                 'title' => 'Some world title 1',
+//                 'content' => 'Some world content 1',
+//                 'date' => 'January 1, 2021',
+//                 'author' => 'Pets',
+//             ],
+//             [
+//                 'title' => 'Some world title 2',
+//                 'content' => 'Some world content 2',
+//                 'date' => 'January 2, 2021',
+//                 'author' => 'Juss',
+//             ],
+//             [
+//                 'title' => 'Some world title 3',
+//                 'content' => 'Some world content 3',
+//                 'date' => 'January 3, 2021',
+//                 'author' => 'Alex',
+//             ],
+//             [
+//                 'title' => 'Some world title 4',
+//                 'content' => 'Some world content 4',
+//                 'date' => 'January 4, 2021',
+//                 'author' => 'Manivald',
+//             ],
+//         ];
+//         include __DIR__ . '/../views/index.php';
+//         break;
+//     case '/us':
+//         $title = 'U.S';
+//         $posts = [
+//             [
+//                 'title' => 'Some U.S title 1',
+//                 'content' => 'Some U.S content 1',
+//                 'date' => 'January 1, 2021',
+//                 'author' => 'Pets',
+//             ],
+//             [
+//                 'title' => 'Some U.S title 2',
+//                 'content' => 'Some U.S content 2',
+//                 'date' => 'January 2, 2021',
+//                 'author' => 'Juss',
+//             ],
+//             [
+//                 'title' => 'Some U.S title 3',
+//                 'content' => 'Some U.S content 3',
+//                 'date' => 'January 3, 2021',
+//                 'author' => 'Alex',
+//             ],
+//             [
+//                 'title' => 'Some U.S title 4',
+//                 'content' => 'Some U.S content 4',
+//                 'date' => 'January 4, 2021',
+//                 'author' => 'Manivald',
+//             ],
+//         ];
+//         include __DIR__ . '/../views/us.php';
+//         break;
+//     default:
+//         echo '404';
+// }
